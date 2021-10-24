6ca8fa7596cf195331aa18d1f2a914ea7512a310
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-10-24 20:12:13 -0300

    Configure for deploy nodejs app on jenkins server

--- terraform/aws/outputs.tf
+++ terraform/aws/outputs.tf
@@ -4,8 +4,10 @@
 
 output "node-app-ip" {
   value = aws_instance.node-app.public_ip
+  description = "Node App Server IP"
 }
 
 output "jenkins-ip" {
   value = aws_instance.jenkins.public_ip
+  description = "Jenkins Server IP"
 }
